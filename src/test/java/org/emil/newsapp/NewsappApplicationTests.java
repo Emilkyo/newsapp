05f51b25c2809c0b6122c14f2a2239968f3fd2f8
Emil <mr.emilka0@gmail.com>
2023-05-13 06:59:22 +0600

    Initial commit

---
+++ src/test/java/org/emil/newsapp/NewsappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emil.newsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
